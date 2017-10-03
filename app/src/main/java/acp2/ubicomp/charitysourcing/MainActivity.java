e07b4527b2fa26c0156b87edd5d2ca23519a8ea6
Ashikur Rahman <asikrhmn@gmail.com>
2017-10-03 15:10:39 +0300

    Initial commit

---
+++ app/src/main/java/acp2/ubicomp/charitysourcing/MainActivity.java
@@ -0,0 +1,13 @@
+package acp2.ubicomp.charitysourcing;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
